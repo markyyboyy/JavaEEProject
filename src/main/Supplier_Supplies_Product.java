0ae20615414ee589c2fae5b1a0ec889a36b29e5e
RichardAllen <allenrichi@hotmail.co.uk>
2016-11-16 15:47:38 +0000

    Named Queries: Supplier_Supplies_Product

--- src/main/Supplier_Supplies_Product.java
+++ src/main/Supplier_Supplies_Product.java
@@ -4,12 +4,17 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Supplier_Supplies_Product")
+
+@NamedQuery(name = "FindBySupplier", query = "SELECT * FROM Supplier_Supplies_Product sSP WHERE sSP.Supplier_id = :Supplier_id ")
+@NamedQuery(name = "FindByProduct", query = "SELECT * FROM Supplier_Supplies_Product sSP WHERE sSP.Product_id = :Product_id ")
+@NamedQuery(name = "FindByPrice", query = "SELECT * FROM Supplier_Supplies_Product sSP WHERE sSP.price = :price ")
 
 
 public class Supplier_Supplies_Product {
