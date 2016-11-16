baaf7d4cdd00cb8b473d20a96fba5dd030795952
ImanAH <iman.hassan@outlook.com>
2016-11-16 14:48:31 +0000

    adding entity

--- src/main/CustomerOrderLine.java
+++ src/main/CustomerOrderLine.java
@@ -1,3 +1,13 @@
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
 @Entity
 @Table(name="CustomerOrderLine")
 public class CustomerOrderLine {
