93daf72b26c7c1008f7a61499ab01bd00109e749
ryanbrook90 <ryanbrook90@gmail.com>
2016-11-16 11:45:45 +0000

    Product, WishList, CustomerOrder classes

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -28,36 +28,74 @@ class Product {
 		this.quantity = quantity;
 	}
 
-	public void setProduct(Product product) {
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
 		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
 	}
 
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	public String getDesc() {
+		return desc;
+	}
+
 	public void setDesc(String desc) {
 		this.desc = desc;
+	}
+
+	public double getPrice() {
+		return price;
 	}
 
 	public void setPrice(double price) {
 		this.price = price;
 	}
 
+	public double getWeight() {
+		return weight;
+	}
+
 	public void setWeight(double weight) {
 		this.weight = weight;
+	}
+
+	public String getColour() {
+		return colour;
 	}
 
 	public void setColour(String colour) {
 		this.colour = colour;
 	}
 
+	public int getSize() {
+		return size;
+	}
+
 	public void setSize(int size) {
 		this.size = size;
 	}
 
+	public int getQuantity() {
+		return quantity;
+	}
+
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [productId=" + productId + ", name=" + name + ", desc=" + desc + ", price=" + price
+				+ ", weight=" + weight + ", colour=" + colour + ", size=" + size + ", quantity=" + quantity + "]";
 	}
 
 }
