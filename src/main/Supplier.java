32d5bfe4b58477784dba575f672aa1aba9dd2140
ImanAH <iman.hassan@outlook.com>
2016-11-16 14:16:18 +0000

    adding entity

--- src/main/Supplier.java
+++ src/main/Supplier.java
@@ -1,4 +1,13 @@
+
+@Entity 
+@Table(name = "Supplier")
+
 public class Supplier {
+	
+	@Id
+	@Column(name ="id", nullable = false, unique = true)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
 	
 	private int idSupplier;
 	
