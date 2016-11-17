c6a2e39bdcd27764454ac4b4ce5d9517e5c7b665
Shrek35 <ynyr_llew@hotmail.com>
2016-11-17 12:23:31 +0000

    fix merge conflicts

--- src/managers/RatingManager.java
+++ src/managers/RatingManager.java
@@ -2,26 +2,20 @@ import java.List;
 import java.Rating;
 
 public interface RatingManager {
-<<<<<<< HEAD
 	//CREATE: CUSTOMER
 	public void createRating(Rating rating);
 	
 	
 	//what if someone buys a product twice
 	//READ: CUSTOMER, VISITOR INVENTORY MANAGER
-=======
-	
-	//CREATE - CUSTOMER
-	public void createRating(Rating rating);
+
 	//READ - CUSTOMER/VISITOR/INV MANAGER
->>>>>>> 1d2b870261de31c734a6802c0d0a7f8f03285aad
 	public Rating findRatingsbyProductCustomerID(int productID, int CustomerID);
 	public List<Rating> findRatingsbyCustomerID(int CustomerID);
 	public List<Rating> findRatingsbyProductID(int productID);
 	public List<Rating> findRatingsbyScore(int Score);
 	
 	//UPDATE	
-	public void updateRating(Rating rating);
-	//what if someone buys a product twice
+	public void updateRating(Rating rating)
 	
 }
--- src/managers/StockManager.java
+++ src/managers/StockManager.java
@@ -1,18 +1,10 @@
 public interface StockManager {
 	
-<<<<<<< HEAD
 	//CREATE, UPDATE: INVENTORY MANAGER
 	public Stock createStock(Stock Stock);
 	public void updateStock(Stock Stock);
 	
-	
 	//READ: VISITOR, CUSTOMER, INVENTORY MANAGER
-=======
-	//CREATE - INV MANAGER
-	public Stock createStock(Stock Stock);
-	
-	//READ - CUSTOMER/VISITOR/INV MANAGER
->>>>>>> 1d2b870261de31c734a6802c0d0a7f8f03285aad
 	public Stock findStocksbyID(int stockID);
 	public List<Stock> findStocksbyPorousware(boolean porous);
 	public List<Stock> findStocksbyRefurbished(boolean refurbished);
@@ -20,5 +12,4 @@ public interface StockManager {
 	//UPDATE-INV MANAGER
 	public void updateStock(Stock Stock);
 	//what if someone buys a product twice could do by order
-	
 }
