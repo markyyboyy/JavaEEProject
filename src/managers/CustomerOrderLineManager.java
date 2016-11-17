c900853877cbc0e57f8ffacc71ef9609459f0a4b
ImanAH <iman.hassan@outlook.com>
2016-11-17 11:41:47 +0000

    edits

--- src/managers/CustomerOrderLineManager.java
+++ src/managers/CustomerOrderLineManager.java
@@ -3,12 +3,12 @@ public interface CustomerOrderLineManager {
 	//CREATE - Customer Only
 	public void createCustomerOrderLine(CustomerOrderLine customerOrderLine);
 	
-	//READ - Custoemr and Inventory Manager Only
+	//READ - Customer and Inventory Manager Only
 	public CustomerOrderLine readbyId(int id);
-	public CustomerOrderLine readByQuantity(int quantity);
-	public CustomerOrderLine readByTotalPrice(int totalPrice);
-	public CustomerOrderLine readByCustomerOrder_idOrder(CustomerOrder CustomerOrder_idOrder);
-	public CustomerOrderLine readByStock_idStock(CustomerOrder Stock_idStock);
+	public List<CustomerOrderLine> readByQuantity(int quantity);
+	public List<CustomerOrderLine> readByTotalPrice(int totalPrice);
+	public List<CustomerOrderLine> readByCustomerOrder_idOrder(CustomerOrder CustomerOrder_idOrder);
+	public List<CustomerOrderLine> readByStock_idStock(CustomerOrder Stock_idStock);
 	
 	//UPDATE - Customer Only
 	public void updateCustomerOrderLine(CustomerOrderLine customerOrderLine);
