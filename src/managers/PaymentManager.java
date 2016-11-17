2c2f4823a4234b2fb3741f10e14df3677eb6d72e
Mark <mark559862@msn.com>
2016-11-17 11:21:53 +0000

    Adding micheals interfaces

--- src/managers/PaymentManager.java
+++ src/managers/PaymentManager.java
@@ -5,4 +5,6 @@ public interface Payment {
 	public Payment readPayment(String cardNumber);
 	public Payment readPayment(String issueNumber);
 	public void updatePayment(String type, String cardNumber,Calender expiryDate, String nameOnCard, String issueNumber, Customer customer, Address address);
+	public Customer findCustomer(int ID);
+	public Address findAddress(int ID);
 }
