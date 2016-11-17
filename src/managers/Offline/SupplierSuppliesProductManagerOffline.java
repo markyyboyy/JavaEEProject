1cd4159e69787134da7a1a8ca0928c03f84d6e0e
RichardAllen <allenrichi@hotmail.co.uk>
2016-11-17 15:49:09 +0000

    SecurityQuestionOffline Added

---
+++ src/managers/Offline/SupplierSuppliesProductManagerOffline.java
@@ -0,0 +1,43 @@
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+import java.util.List;
+
+@Stateless
+@Default
+public class SupplierSuppliesProductManagerOffline implements SupplierSuppliesProductManager{
+	
+	@Inject
+	private TestData testData;
+	
+	//CREATE - INVENTORY MANAGER ONLY
+	@Override
+	public void createSupplierSuppliesProduct(Supplier_Supplies_Product newSSP){
+		List<Supplier_Supplies_Product> SSPOrders = testData.getSupplierSuppliesProducts();
+		SSPOrders.add(newSSP);
+		testData.setSupplierSuppliesProducts(SSPOrders);
+	}
+	
+	//READ - INVENTORY MANAGER ONLY
+	@Override
+	public Supplier_Supplies_Product readSupplierSuppliesProductByProductID(int pID){
+		for (Supplier_Supplies_Product SSP : testData.getSupplierSuppliesProducts()) {
+			if (SSP.getProduct().getProductId() == pID) {
+				return SSP;
+			}
+			return null;
+		}
+	}
+	
+	@Override
+	public Supplier_Supplies_Product readSupplierSuppliesProductBySupplierID(int sID){
+		for (Supplier_Supplies_Product SSP : testData.getSupplierSuppliesProducts()) {
+			if (SSP.getSupplier().getIdSupplier() == sID) {
+				return SSP;
+			}
+			return null;
+		}
+	}
+
+}
