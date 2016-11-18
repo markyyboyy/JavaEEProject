32fa9c675485d8d4859e125e690365570e290378
Mark <mark559862@msn.com>
2016-11-18 11:34:24 +0000

    Correcting SupplierManager

--- src/main/java/com/qac/row5project/managers/offline/SupplierManagerOffline.java
+++ src/main/java/com/qac/row5project/managers/offline/SupplierManagerOffline.java
@@ -1,18 +1,27 @@
 package com.qac.row5project.managers.offline;
+import java.util.ArrayList;
+import java.util.function.Supplier;
+
 import javax.ejb.Stateless;
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
+import com.qac.row5project.entities.TestData;
+import com.qac.row5project.managers.SupplierManager;
+
 @Stateless
 @Default
 
-public class SupplierManagerOffline implements SupplierManger {
+public class SupplierManagerOffline implements SupplierManager {
 	@Inject
 	private TestData testData;
 	
 	//CREATE - Inventory Manager Only
 	@Override
-	public void createSupplier(Supplier supplier);
+	public void createSupplier(Supplier supplier){
+		ArrayList<Supplier> list = testData.getSuppliers();
+		
+	}
 		
 		
 	//READ - Inventory Manager Only
