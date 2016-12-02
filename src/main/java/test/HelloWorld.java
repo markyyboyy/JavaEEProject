a66b2ae70c7390c8c3d29d8fe17f6acfe792dde8
mcrowther93 <michaelcrowther09@gmail.com>
2016-12-02 12:30:49 +0000

    ?

---
+++ src/main/java/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package test;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name = "helloWorld", eager = true)
+public class HelloWorld {
+   public HelloWorld() {
+      System.out.println("HelloWorld started!");
+   }
+	
+   public String getMessage() {
+      return "Hello World!";
+   }
+}
