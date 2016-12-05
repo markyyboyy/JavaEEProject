e735d9c15c14b257c1528bdcf9a33bffb37d236b
ryanbrook90 <ryanbrook90@gmail.com>
2016-12-05 11:49:31 +0000

    annotations customerordermanageroffline

--- src/main/java/com/qac/row5project/managers/offline/CustomerOrderManagerOffline.java
+++ src/main/java/com/qac/row5project/managers/offline/CustomerOrderManagerOffline.java
@@ -1,3 +1,6 @@
+/**
+ * @Author RyanB
+ */
 package com.qac.row5project.managers.offline;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -20,6 +23,7 @@ public class CustomerOrderManagerOffline implements CustomerOrderManager {
 	
 	//CREATE - CUSTOMER
 	@Override
+	//RETUSN A CUSTOMER ORDER
 	public CustomerOrder createCustomerOrder(CustomerOrder co) {
 		ArrayList<CustomerOrder> co1 = testData.getCustomerOrders();
 		co.setCustomerOrderId(co1.size()+1);
@@ -30,6 +34,7 @@ public class CustomerOrderManagerOffline implements CustomerOrderManager {
 	
 	//UPDATE - CUSTOMER
 	@Override
+	//UPDATES CUSTOMER ORDER DETAILS BASED ON THE ORDER THAT IS PASSED TO IT
 	public void updateCustomerOrder(CustomerOrder co){
 		List<CustomerOrder> co1 = testData.getCustomerOrders();
 		for(int i=0;i<co1.size();i++)
@@ -39,6 +44,7 @@ public class CustomerOrderManagerOffline implements CustomerOrderManager {
 	
 	//READ - CUSTOMER/INVENTORY MANAGER
 	@Override
+	//ALLOWS A CUSTOMER ORDER TO BE VIEWED BASED ON THE ORDER ID
 	public CustomerOrder readCustomerOrderById(long id) {
 			for(CustomerOrder co : testData.getCustomerOrders())
 				if(co.getCustomerId() == id)
@@ -47,6 +53,7 @@ public class CustomerOrderManagerOffline implements CustomerOrderManager {
 	}
 	
 	@Override
+	//ALLOWS A CUSTOMER ORDER TO BE VIEWED BASED ON THE ORDER DATE
 	public CustomerOrder readCustomerOrderByDatePlaced(Calendar date){
 		for(CustomerOrder co : testData.getCustomerOrders())
 			if(co.getDatePlaced() == date)
