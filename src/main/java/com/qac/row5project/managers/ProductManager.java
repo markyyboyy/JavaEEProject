9dd8fd95b79bf0e9981a672aedefeab6da4eb95f
Shrek35 <ynyr_llew@hotmail.com>
2016-12-05 15:29:29 +0000

    edited name in productManager

--- src/main/java/com/qac/row5project/managers/ProductManager.java
+++ src/main/java/com/qac/row5project/managers/ProductManager.java
@@ -1,10 +1,9 @@
 /**
- * @Author RyanB
+ * @Author RyanB(Ynyr edited)
  */
 
 package com.qac.row5project.managers;
-import java.util.ArrayList;
-
+import java.util.List;
 import com.qac.row5project.entities.Product;
 
 public interface  ProductManager {
@@ -18,8 +17,8 @@ public interface  ProductManager {
 	
 	//READ - CUSTOMER/INVENTORY MANAGER
 	public Product readProductById(long id);
-	public Product readProductByName(String name);
-	public ArrayList<Product> readProductByColour(String colour);
+	public List<Product> readProductByName(String name);
+	public List<Product> readProductByColour(String colour);
 	
 	
 }
