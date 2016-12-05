23de83b0e971462ea34ef11fd5644a21c7d6630d
RichardAllen <allenrichi@hotmail.co.uk>
2016-12-05 16:15:11 +0000

    added login testData

--- src/main/java/test/CurrentUser.java
+++ src/main/java/test/CurrentUser.java
@@ -1,5 +1,32 @@
 package test;
 
-public class CurrentUser {
+import java.io.Serializable;
 
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+import com.qac.row5project.entities.Customer;
+
+@Named("user")
+@SessionScoped
+public class CurrentUser implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Customer customer = null;
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+	
+	public Customer getCustomer()
+	{
+		return this.customer;
+	}
+	
+	public boolean isLoggedIn(){
+		return (customer ==null) ? true : false;
+	}
 }
