3479efc43d7b20d0fa50bdd76a3b36485d34c3f1
RichardAllen <allenrichi@hotmail.co.uk>
2016-12-05 13:45:58 +0000

    added comments to productSupplierManager

--- src/main/java/com/qac/row5project/managers/ProductSupplierManager.java
+++ src/main/java/com/qac/row5project/managers/ProductSupplierManager.java
@@ -11,10 +11,26 @@ import com.qac.row5project.entities.ProductSupplier;
 public interface ProductSupplierManager {
 	
 	//CREATE - INVENTORY MANAGER ONLY
+	/** 
+	 * 
+	 * @author Richard Allen
+	 * Create new supplier with an ID 
+	 * 
+	 */
 	public void createProductSupplier(ProductSupplier p);	
 	
 	//READ - INVENTORY MANAGER ONLY
+	/** 
+	 * 
+	 * @author Richard Allen
+	 * @return supplier by product ID
+	 */
 	public ProductSupplier readProductSupplierByProductID(int pID);
+	/** 
+	 * 
+	 * @author Richard Allen
+	 * @return Supplier by supplier ID
+	 */
 	public ProductSupplier readProductSupplierBySupplierID(int sID);
 
 }
