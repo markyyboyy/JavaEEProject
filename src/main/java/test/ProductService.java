53ff658014c6ab7f002a8888ae5123739c3a616c
ryanbrook90 <ryanbrook90@gmail.com>
2016-12-05 14:00:12 +0000

    annotations for product.html/xhtml

---
+++ src/main/java/test/ProductService.java
@@ -0,0 +1,37 @@
+package test;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import com.qac.row5project.entities.Product;
+import com.qac.row5project.managers.ProductManager;
+import com.qac.row5project.managers.StockManager;
+
+@Stateless
+public class ProductService {
+	
+	@Inject
+	private ProductManager productManager;
+	@Inject
+	private StockManager stockManager;
+	
+
+	public Product readProductByName(String name) {
+	try {
+	return readProductById(Long.parseLong(name));
+	
+	} catch (Exception e) {
+	return null;}
+	}
+	
+
+	
+	public Product readProductById(long id) {
+		return readProductById(id);
+		}
+
+	
+		  }
+
+	
+	
