60fef9e6a8804ea9f5854e6674312c052b4c7c2d
RichardAllen <allenrichi@hotmail.co.uk>
2016-12-05 13:47:16 +0000

    added comments to securityQuestionManager

--- src/main/java/com/qac/row5project/managers/SecurityQuestionManager.java
+++ src/main/java/com/qac/row5project/managers/SecurityQuestionManager.java
@@ -11,12 +11,27 @@ import com.qac.row5project.entities.SecurityQuestion;
 public interface SecurityQuestionManager {
 
 	//CREATE - CUSTOMER ONLY
+	/** 
+	 * 
+	 * @author Richard Allen
+	 * Create new security question
+	 */
 	public void createSecurityQuestion(SecurityQuestion newQuestion);
 	
 	//READ - CUSTOMER ONLY
+	/** 
+	 * 
+	 * @author Richard Allen
+	 * @return the security question
+	 */
 	public SecurityQuestion readSecurityQuestion(String question);	
 	
 	//UPDATE - CUSTOMER ONLY
+	/** 
+	 * 
+	 * @author Richard Allen
+	 * update the security question
+	 */
 	public void updateSecurityQuestion(String newQuestion, String oldQuestion);
 	
 }
