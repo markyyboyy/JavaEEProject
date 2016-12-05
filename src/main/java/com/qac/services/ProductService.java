aaeb3479d6356ea51e5f380642ee008887284beb
ryanbrook90 <ryanbrook90@gmail.com>
2016-12-05 14:11:53 +0000

    restrucuture product service

--- src/main/java/com/qac/services/ProductService.java
+++ src/main/java/com/qac/services/ProductService.java
@@ -1,19 +1,44 @@
 package com.qac.services;
+
+import java.util.ArrayList;
+
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
-import com.qac.row5project.managers.WishlistManager;
+import com.qac.row5project.entities.Product;
+import com.qac.row5project.managers.ProductManager;
+import com.qac.row5project.managers.StockManager;
 
 @Stateless
 public class ProductService {
-@Inject
-private WishlistManager wishlistManager;
-public boolean validLogin(String email, String password) {
-	Customer customer = customerManager.findByEmail(email.toLowerCase());
-	return (customer.getPassword().equals(password))? true : false;
-}
+	
+	@Inject
+	private ProductManager productManager;
+	@Inject
+	private StockManager stockManager;
+	
 
-public Customer loginUser(String email) {
-	return customerManager.findByEmail(email.toLowerCase());
-}
-}
+	public Product readProductByName(String name) {
+	try {
+	return readProductById(Long.parseLong(name));
+	
+	} catch (Exception e) {
+	return null;}
+	}
+		
+	public Product readProductById(long id) {
+		return productManager.readProductById(id);
+		}
+	
+	public ArrayList<Product> readProductByColour(String colour){
+		try {
+			return productManager.readProductByColour(colour);
+			} catch (Exception e) {
+			return null;}
+			}
+	
+	public Product getProduct(long id) {
+		return getProductItem(productManager.findById(id), stockManager.findById(id));
+
+	}
+		
