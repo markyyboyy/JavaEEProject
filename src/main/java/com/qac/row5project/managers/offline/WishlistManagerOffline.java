0d7c8d1a4cec0077f1922efc08157ee9c1f93c41
Mark <mark559862@msn.com>
2016-12-06 13:13:46 +0000

    fix merge

--- src/main/java/com/qac/row5project/managers/offline/WishlistManagerOffline.java
+++ src/main/java/com/qac/row5project/managers/offline/WishlistManagerOffline.java
@@ -4,16 +4,13 @@
  *
  */package com.qac.row5project.managers.offline;
 import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.enterprise.inject.Default;
 import javax.inject.Inject;
 
-<<<<<<< HEAD
 import com.qac.row5project.entities.Customer;
 import com.qac.row5project.entities.Product;
-=======
-import com.qac.row5project.entities.Stock;
->>>>>>> f9d42f309a0b57702bb318e883d7088812bb4444
 import com.qac.row5project.entities.TestData;
 import com.qac.row5project.managers.WishlistManager;
 
@@ -23,8 +20,6 @@ import com.qac.row5project.managers.WishlistManager;
 public class WishlistManagerOffline implements WishlistManager {
 	@Inject
 	private TestData testData;
-<<<<<<< HEAD
-	
 
 	//READ - Customer Only
 	@Override
@@ -49,20 +44,4 @@ public class WishlistManagerOffline implements WishlistManager {
 			}
 		}
 	}
-=======
-
-	@Override
-	public List<Stock> findByCustomerId(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<Stock> readWishlist(long id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
->>>>>>> f9d42f309a0b57702bb318e883d7088812bb4444
 }
