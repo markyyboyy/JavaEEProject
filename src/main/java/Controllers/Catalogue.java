aaf89e21d13dfbdff424cfd20c873ae0b359cc82
ryanbrook90 <ryanbrook90@gmail.com>
2016-12-06 09:31:08 +0000

    changes test package name

--- src/main/java/Controllers/Catalogue.java
+++ src/main/java/Controllers/Catalogue.java
@@ -1,4 +1,4 @@
-package test;
+package Controllers;
 
 import java.io.Serializable;
 import java.util.ArrayList;
--- src/main/java/Controllers/HelloWorld.java
+++ src/main/java/Controllers/HelloWorld.java
@@ -2,7 +2,7 @@
  * @author Mark Freeman
  * This class was used for testing, just to see if I could get a bean working with XHTML. It has no role in the final system.
  */
-package test;
+package Controllers;
 import javax.faces.bean.ManagedBean;
 
 @ManagedBean(name = "helloWorld", eager = true)
