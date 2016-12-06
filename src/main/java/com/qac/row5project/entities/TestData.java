a27260eb913703058a2afc2177d82bdf7aff1a0e
RichardAllen <allenrichi@hotmail.co.uk>
2016-12-06 09:42:05 +0000

    fixed testData

--- src/main/java/com/qac/row5project/entities/TestData.java
+++ src/main/java/com/qac/row5project/entities/TestData.java
@@ -54,14 +54,7 @@ public class TestData {
 		productSuppliers = new ArrayList<ProductSupplier>();
 		suppliers = new ArrayList<Supplier>();
 		wishlists = new ArrayList<WishList>();
-		WishList list = new WishList();
-		ArrayList<ProductItemTest> products = new ArrayList<ProductItemTest>();
-		ProductItemTest p = new ProductItemTest();
-		
-		p.setTitle("A gnome");
-		products.add(p);
-		list.setProducts(products);
-		wishlists.add(list);
+
 		
 		LoginDetails testLogin = new LoginDetails("test@test.com", "password");
 		loginDetails.add(testLogin);
