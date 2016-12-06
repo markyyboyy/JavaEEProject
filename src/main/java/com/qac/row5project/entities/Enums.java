73c8a08757670d0e4ce667683425b9b43abea9f3
Mark <mark559862@msn.com>
2016-12-06 15:04:13 +0000

    enums

---
+++ src/main/java/com/qac/row5project/entities/Enums.java
@@ -0,0 +1,14 @@
+package com.qac.row5project.entities;
+
+public enum Enums {
+	
+		/**
+		 * The product is active
+		 */
+		ACTIVE, 
+		/**
+		 * Is no longer sold
+		 */
+		DISCONTINUED, SPRING, SUMMER, AUTUM, WINTER
+	}
+
