f1da564884676079a342bb62287a5b03469a2ace
Shrek35 <ynyr_llew@hotmail.com>
2016-12-07 17:20:54 +0000

    f

--- src/main/java/com/qac/row5project/entities/Enums.java
+++ src/main/java/com/qac/row5project/entities/Enums.java
@@ -1,21 +1,4 @@
-<<<<<<< HEAD
 
-package com.qac.row5project.entities;
-
-public class Enums {
-	public enum ProductStatus {
-
-/**
- * @Author RyanB
- */
-
-||||||| merged common ancestors
-<<<<<<< HEAD
-package com.qac.row5project.entities;
-
-public class Enums {
-	public enum ProductStatus {
-=======
 /**
  * @Author RyanB
  */
@@ -23,40 +6,10 @@ public class Enums {
 package com.qac.row5project.entities;
 
 public enum Enums {
-	
->>>>>>> b97ce2ad8cfa6bcf9d0dc67f5776ad97f2945969
-=======
-package com.qac.row5project.entities;
 
-public enum Enums {
->>>>>>> a910c8386a43aaaa9452831be3a13b538d4cfeac
-		/**
-		 * The product is active
-		 */
-		/**
-		 * Is no longer sold
-		 */
-		/**
-		 * Will be in stock soon, new product line
-		 */
-<<<<<<< HEAD
-		 // Arriving soon, new product line
+
+	//	 * Arriving soon, new product line
 	//	 */
 	//	COMINGSOON
 	}
 
-||||||| merged common ancestors
-		COMINGSOON
-	}
-}
-=======
-		 * Arriving soon, new product line
-		 */
-		COMINGSOON
-	}
-
->>>>>>> b97ce2ad8cfa6bcf9d0dc67f5776ad97f2945969
-=======
-		COMINGSOON
-	}
->>>>>>> a910c8386a43aaaa9452831be3a13b538d4cfeac
