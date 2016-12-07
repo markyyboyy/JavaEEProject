f8b02d43ffa5f492034c5e1b20fdfbb1e27bdc56
Mark <mark559862@msn.com>
2016-12-07 16:39:48 +0000

    Finished implementing GeneratedOrder. Yet to clean and comment code.

--- src/main/java/com/qac/row5project/entities/Enums.java
+++ src/main/java/com/qac/row5project/entities/Enums.java
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-package com.qac.row5project.entities;
-
-public class Enums {
-	public enum ProductStatus {
-=======
-/**
- * @Author RyanB
- */
-
 package com.qac.row5project.entities;
 
 public enum Enums {
-	
->>>>>>> b97ce2ad8cfa6bcf9d0dc67f5776ad97f2945969
 		/**
 		 * The product is active
 		 */
@@ -22,16 +10,7 @@ public enum Enums {
 		 */
 		DISCONTINUED,
 		/**
-<<<<<<< HEAD
 		 * Will be in stock soon, new product line
 		 */
 		COMINGSOON
 	}
-}
-=======
-		 * Arriving soon, new product line
-		 */
-		COMINGSOON
-	}
-
->>>>>>> b97ce2ad8cfa6bcf9d0dc67f5776ad97f2945969
