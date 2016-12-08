cd3bf742a924f60c02bce03d6adff0d1ed214da9
Mark <mark559862@msn.com>
2016-12-08 15:49:21 +0000

    please work

--- src/main/java/com/qac/row5project/entities/CustomerOrder.java
+++ src/main/java/com/qac/row5project/entities/CustomerOrder.java
@@ -60,27 +60,24 @@ public class CustomerOrder {
 	};
 	
 	
-	public CustomerOrder(int customerOrderId, int customerId, Calendar datePlaced,int addressId, int paymentId) {
+	public CustomerOrder(int customerOrderId, int customerId, Calendar datePlaced,int addressId) {
 		super();
 		this.customerOrderId = customerOrderId;
 		this.customerId = customerId;
 		this.datePlaced = datePlaced;
 		this.addressId = addressId;
-		this.paymentId = paymentId;
 		deriveTotalPrice();
 		
 	}
 	//CONSTRUCTOR FOR CUSTOMER ORDER THAT TAKES ALL VARIABLES
-	public CustomerOrder(int customerOrderId, int feedbackId, int customerId, Calendar datePlaced,int addressId, int paymentId) {
+	public CustomerOrder(int customerOrderId, int feedbackId, int customerId, Calendar datePlaced,int addressId) {
 		super();
 		this.customerOrderId = customerOrderId;
 		this.feedbackId = feedbackId;
 		this.customerId = customerId;
 		this.datePlaced = datePlaced;
 		this.addressId = addressId;
-		this.paymentId = paymentId;
 		deriveTotalPrice();
-		
 	}
 	public void deriveTotalPrice(){
 		totalPrice=0;
@@ -161,13 +158,6 @@ public class CustomerOrder {
 		this.addressId = addressId;
 	}
 
-	public int getPaymentId() {
-		return paymentId;
-	}
-
-	public void setPaymentId(int paymentId) {
-		this.paymentId = paymentId;
-	}
 
 
 	@Override
