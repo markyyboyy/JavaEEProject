632e846af211a5c2c2ea714d40c6737fa032b29a
mcrowther93 <michaelcrowther09@gmail.com>
2016-12-09 15:01:26 +0000

    Comments

--- src/main/java/com/qac/row5project/helpers/PaginationHelper.java
+++ src/main/java/com/qac/row5project/helpers/PaginationHelper.java
@@ -13,19 +13,44 @@ public abstract class PaginationHelper {
 
 	}
 
+	/**
+	 * 
+	 * @return the amount of Item within the DataModel
+	 */
 	public abstract int getItemsCount();
 
+	/**
+	 * 
+	 * @return the list of items to be used within 
+	 * the pagination. M
+	 * 
+	 * Model takes any Object Type
+	 */
 	public abstract DataModel<?> createPageDataModel();
 
+	/***
+	 * 
+	 * @return item to be shown per page
+	 */
 	public int getPageSize() {
 		return pageSize;
 	}
 
+	/***
+	 * 
+	 * @param pageSize how many items to be seen per page
+	 */
 	public void setPageSize(int pageSize) {
 		this.pageSize = pageSize;
 
 	}
 
+	
+	/***
+	 * 
+	 * @return the the index of the first item to shown on the 
+	 * view
+	 */
 	public int getPageFirstItem() {
 
 		return page * pageSize;
@@ -46,12 +71,18 @@ public abstract class PaginationHelper {
 		return i;
 
 	}
-
+	/***
+	 * 
+	 * @return if there are more item to be seen
+	 */
 	public boolean isHasNextPage() {
 
 		return (page + 1) * pageSize + 1 <= getItemsCount();
 	}
 
+	/**
+	 * Go to next page to view the additional items
+	 */
 	public void nextPage() {
 
 		if (isHasNextPage())
