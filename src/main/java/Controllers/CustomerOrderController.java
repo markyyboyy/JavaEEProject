001450302629b5c033e47ee2adc41d7353506194
Shrek35 <ynyr_llew@hotmail.com>
2016-12-09 09:27:25 +0000

    merge

--- src/main/java/Controllers/CustomerOrderController.java
+++ src/main/java/Controllers/CustomerOrderController.java
@@ -36,17 +36,9 @@ public class CustomerOrderController implements Serializable {
 		setCustomerOrders(customerOrderService.readCustomerOrders());
 		
 		return new ListDataModel<>(lsOrders);
-
 	}
-	
-	
-
-		
-		
 
 	public void setCustomerOrders(List<CustomerOrder> lstTemp ){
 		this.lsOrders = lstTemp;
 	}
-		
-		
 }
