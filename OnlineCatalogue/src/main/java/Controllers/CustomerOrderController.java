3c9284577fb01f8201fcc8e9b8f294ec3ae4a948
RichardAllen <allenrichi@hotmail.co.uk>
2016-12-16 11:50:08 +0000

    testdata + order controllers updates

---
+++ OnlineCatalogue/src/main/java/Controllers/CustomerOrderController.java
@@ -0,0 +1,22 @@
+package Controllers;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.qac.row5project.managers.CustomerManager;
+import com.qac.services.LoginService;
+
+import Controllers.session.CurrentUser;
+
+/**
+ * 
+ * @author Richard Allen
+ *
+ */
+
+@Named("customerOrder")
+@RequestScoped
+public class CustomerOrderController {
+
+}
---
+++ OnlineCatalogue/src/main/java/com/qac/services/CustomerOrderService.java
@@ -0,0 +1,21 @@
+package com.qac.services;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import com.qac.row5project.entities.Customer;
+import com.qac.row5project.entities.LoginDetails;
+import com.qac.row5project.managers.CustomerManager;
+
+import com.qac.row5project.managers.LoginDetailsManager;
+
+/**
+ * 
+ * @author Richard Allen
+ *
+ */
+
+@Stateless
+public class CustomerOrderService {
+
+}
