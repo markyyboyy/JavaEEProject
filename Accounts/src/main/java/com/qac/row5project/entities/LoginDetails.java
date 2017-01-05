8b1c103380b325ddf4460957585b26ec271a78f9
Mark <mark559862@msn.com>
2017-01-05 14:37:05 +0000

    Adding accounts project

---
+++ Accounts/src/main/java/com/qac/row5project/entities/LoginDetails.java
@@ -0,0 +1,53 @@
+/**
+ * @author Mark Freeman
+ */
+package com.qac.row5project.entities;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="login Details")
+public class LoginDetails {
+	/**
+	The table has two columns
+	 */
+	@Column (name="email",length=100,nullable=false,unique=true)
+	@Size(max = 100)
+	private String email;
+	@Column (name="password",length=100,nullable=false)
+	@Size(max = 100)
+	private String password;
+	
+
+	public LoginDetails() {
+	}
+	/**
+	To construct a user you need a username and a password
+	 */
+	
+	//params
+	public LoginDetails(String email, String password) { 
+		this.email = email;
+		this.password = password;
+	}
+	/**
+	Gettters and setters
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
---
+++ Accounts/src/main/java/com/qac/row5project/managers/offline/LoginDetailsManagerOffline.java
@@ -0,0 +1,55 @@
+/**
+ * @author Mark Freeman
+ */
+package com.qac.row5project.managers.offline;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.enterprise.inject.Default;
+import javax.inject.Inject;
+
+import com.qac.row5project.entities.LoginDetails;
+import com.qac.row5project.helpers.*;
+import com.qac.row5project.managers.LoginDetailsManager;
+
+@Stateless
+@Default
+
+public class LoginDetailsManagerOffline implements LoginDetailsManager {
+	@Inject
+	private TestDataIMS testData;
+	
+	//CREATE - Inventory Manager Only
+	@Override
+	public void createLoginDetails(LoginDetails loginDetails){	//Add a user to the database
+		List<LoginDetails> returned = testData.getLoginDetails();
+		returned.add(loginDetails);	//Construct the updated ArrayList.
+		testData.setLoginDetails(returned);	//Replace the ArrayList in the database.
+	}
+	@Override
+	public LoginDetails readLoginDetails(String emailAddress){
+		List<LoginDetails> returned = testData.getLoginDetails();
+		for(LoginDetails l: returned){	//For every user account in the list.
+			if (l.getEmail().equals(emailAddress)){	//When we find a useraccount with the given email address return it.
+				return l;
+			}
+		}
+		return null;	//If we didnt find an account return null.
+	}
+
+	@Override
+	public void updateLoginDetails(LoginDetails logInDetails) {
+		// TODO Auto-generated method stub
+		List<LoginDetails> returned = testData.getLoginDetails();	
+		int i = 0;	//Used as a counter variable.
+		for(LoginDetails l: returned){	//For every user account in the list.
+			if (l.getEmail().equals(logInDetails.getEmail())){	//Find the old version of the acccount.
+				returned.remove(i);	//Remove the old user account.
+				returned.add(logInDetails);	//Add the updated user account.
+			}
+			i++;
+		}
+		testData.setLoginDetails(returned);	//Update the datbase with the new user account list.
+	}
+}
+	
